3b6e30f3227cac9c42ded6fd24adbf4459f018e1
BillLin1209 <zzx123bill@gmail.com>
2020-07-27 22:30:02 +0800

    new repo for chatroom advanced

---
+++ src/test/java/com/bill/ChatroomAdvanced/ChatroomAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.ChatroomAdvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
