b5e428ed1714205d01c0e030514a7c4f482e0b50
BillLin1209 <zzx123bill@gmail.com>
2020-07-28 00:13:44 +0800

    logback.xml

--- src/main/java/com/bill/ChatroomAdvanced/controller/LoginController.java
+++ src/main/java/com/bill/ChatroomAdvanced/controller/LoginController.java
@@ -1,5 +1,11 @@
 package com.bill.ChatroomAdvanced.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LoginController {
+
+	
+	Logger logger=LoggerFactory.getLogger(getClass());
 
 }
